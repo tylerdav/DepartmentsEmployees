6bb1c3c45b47395477ad65be1cef35422138fca9
tylerdav <tdavis1433@att.net>
2020-03-23 13:46:19 -0500

    first commit

---
+++ DepartmentsEmployees/Models/Department.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DepartmentsEmployees.Models
+{
+    // C# representation of the Department table
+    public class Department
+    {
+        public int Id { get; set; }
+        public string DeptName { get; set; }
+        public List<Employee> Employees { get; set; }
+    }
+}
---
+++ DepartmentsEmployees/Program.cs
@@ -0,0 +1,42 @@
+﻿using DepartmentsEmployees.Models;
+using DepartmentsEmployees.Data;
+using System;
+using System.Collections.Generic;
+
+namespace DepartmentsEmployees
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DepartmentRepository departmentRepo = new DepartmentRepository();
+
+            Console.WriteLine("Getting All Departments:");
+            Console.WriteLine();
+
+            List<Department> allDepartments = departmentRepo.GetAllDepartments();
+
+            foreach (Department dept in allDepartments)
+            {
+                Console.WriteLine($"{dept.Id} {dept.DeptName}");
+            }
+
+            Console.WriteLine("----------------------------");
+            Console.WriteLine("Getting Department with Id 1");
+
+            Department singleDepartment = departmentRepo.GetDepartmentById(1);
+
+            Console.WriteLine($"{singleDepartment.Id} {singleDepartment.DeptName}");
+
+            Department legalDept = new Department
+            {
+                DeptName = "Legal"
+            };
+
+            departmentRepo.AddDepartment(legalDept);
+
+            Console.WriteLine("-------------------------------");
+            Console.WriteLine("Added the new Legal Department!");
+        }
+    }
+}
