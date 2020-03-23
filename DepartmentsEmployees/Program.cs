a7fbb5f0aee3485c4881c0ad8e94663b6d989a90
tylerdav <tdavis1433@att.net>
2020-03-23 15:24:05 -0500

    fixed error

--- DepartmentsEmployees/Program.cs
+++ DepartmentsEmployees/Program.cs
@@ -1,7 +1,7 @@
-﻿using DepartmentsEmployees.Models;
-using DepartmentsEmployees.Data;
-using System;
+﻿using System;
 using System.Collections.Generic;
+using DepartmentsEmployees.Models;
+using DepartmentsEmployees.Data;
 
 namespace DepartmentsEmployees
 {
